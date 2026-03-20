94b7861500b8b3b3a96a12e8c3010514ff93a29c
Karla Renata <renatakarla663@gmail.com>
2026-03-20 15:59:59 -0300

    coverage

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,37 @@
 module.exports = {
-  mode: 'production',
+  mode: process.env.NODE_ENV || 'production',
   entry: './app.js',
-  output: { filename: 'bundle.min.js' },
+  output: {
+    filename: 'bundle.[contenthash:8].min.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true  // Limpa dist/ antes de build
+  },
   module: {
     rules: [
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ]
-  }
+  },
+  devServer: {
+    static: './',
+    port: 8080,
+    compress: true
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    })
+  ]
 };
